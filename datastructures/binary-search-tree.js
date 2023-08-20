6a38c7e5d3e12a917c428f2f6360154c5cee13a1
Alan Sax <alan.sax@panerabread.com>
2023-08-19 20:10:24 -0500

    breadth first search

--- datastructures/binary-search-tree.js
+++ datastructures/binary-search-tree.js
@@ -10,6 +10,8 @@
 // ---- if no value, creates new node
 // ---- if value, check if greater or less than and repeat
 
+const Queue = require("./queue")
+
 class Node {
     constructor(value) {
         this.value = value
@@ -31,7 +33,7 @@ class BinarySearchTree {
             return this
         } else {
             var currentNode = this.root
-            while(currentNode != null) {
+            while (currentNode != null) {
                 if (value == currentNode.value) return undefined // do not allow for duplicates
                 const isLessThanAndHasAnotherNode = value < currentNode.value && currentNode.left
                 const isLessThanAndDoesNotHaveAnotherNode = value < currentNode.value && !currentNode.left
@@ -58,7 +60,7 @@ class BinarySearchTree {
         if (this.root == null) return false
 
         var currentNode = this.root
-        while(currentNode != null) {
+        while (currentNode != null) {
             const isEqual = value == currentNode.value
             const isLessThanAndHasAnotherNode = value < currentNode.value && currentNode.left
             const isLessThanAndDoesNotHaveAnotherNode = value < currentNode.value && !currentNode.left
@@ -78,6 +80,39 @@ class BinarySearchTree {
         }
 
         return false
+    }
+
+    // ==========================
+    // BFS - HOW IT WORKS
+    // ==========================
+    //        example tree
+    //             10
+    //          6     15
+    //        3   8     20
+    // ===========================
+    // we start from the top, and go left -> right
+    // queue = [10]
+    // loop over queue, move values to visited, and add left / right values to queue
+    // -- while 10 is in the queue, check if there is a left & right
+    // -- queue = [6, 15]  | visited = [10]
+    // -- queue = [3,8,20] | visited = [10,6,15]
+    // -- queue = [] | visited = [10,6,15,3,8,20]
+    // when queue is empty....done
+    // if find value, done
+    breadthFirstSearch(value) {
+        const queue = new Queue() // this could also be a array -> use shift + unshift
+        const visited = []
+        queue.enqueue(this.root)
+
+        while (queue.size != 0) {
+            const { left, right, value } = queue.dequeue()
+            visited.push(value)
+
+            if (left) queue.enqueue(left)
+            if (right) queue.enqueue(right)
+        }
+
+        return visited
     }
 }
 
--- datastructures/tests/binary-search-tree.test.js
+++ datastructures/tests/binary-search-tree.test.js
@@ -78,3 +78,17 @@ test("Can add create a root, parent with 2 child nodes Binary Search Tree", () =
     expect(binarySearchTree.find(2800)).toStrictEqual(false);
     expect(binarySearchTree.find(20)).toStrictEqual(true);
 });
+
+test("Breadth First Search on a Binary Tree", () => {
+    const binarySearchTree = new BinarySearchTree()
+    binarySearchTree.insert(10)
+    binarySearchTree.insert(15)
+    binarySearchTree.insert(20)
+    binarySearchTree.insert(6)
+    binarySearchTree.insert(8)
+    binarySearchTree.insert(3)
+
+    expect(binarySearchTree.breadthFirstSearch()).toStrictEqual([
+        10, 6, 15, 3, 8, 20
+    ]);
+});
